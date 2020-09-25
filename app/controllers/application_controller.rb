0a2fd487f5a795fc33fcd2d280c47b88bb6c8a65
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-25 18:27:09 +0900

    新規登録機能の実装
    devise_parameter_sanitizerメソッド
    新規登録ページのHTML修正(name属性をカラム名にした)
    form_withメソッドのパス名を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def basic_auth
@@ -7,4 +8,15 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:ja_first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:ja_family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_035212) do
+ActiveRecord::Schema.define(version: 2020_09_25_065853) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_09_25_035212) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.string "first_name"
+    t.string "family_name"
+    t.string "ja_first_name"
+    t.string "ja_family_name"
+    t.date "birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
