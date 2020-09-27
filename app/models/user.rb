f82efa2dff7ee34d9ef6635eb906bea31ec555c9
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-28 01:23:48 +0900

    Userモデルの単体テストコードの作成
    パスワードは正規表現にて条件を設定した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true, length: { maximum: 40 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,62 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "同じメールアドレスは登録できない" do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email 
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+    it "メールアドレスは@を含まないと登録できない" do
+      @user.email = "hogefugagmail.com"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
+    it "パスワードが空では登録できない" do
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it "パスワードが5文字以下だと登録できない" do
+      @user.password = "abc12"
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it "パスワードは数字だけでは登録できない" do
+      @user.password = "123456"
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+    end
+    it "パスワードは英字だけでは登録できない" do
+      @user.password = "abcdef"
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+    end
+    it "パスワードは確認用のパスワードが空だと登録できない" do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
 
+  end
 end
 
 
