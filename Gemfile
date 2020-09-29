b4f657ac1439cc20b5bb55c7981e2c2c764db3b5
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-29 15:33:49 +0900

    mini_magick, image_processing, Active storageインストール, items_controller追記、アソシエーション(itemモデル)

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'pry-rails'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
 
 group :production do
   gem 'rails_12factor'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  has_many :comments
+  has_one :purchase_item
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
