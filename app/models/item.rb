705ec6a8c66e23915ceed8b782e883078123d121
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-29 21:27:29 +0900

    JSを用いた手数料と利益計算、name属性の修正(new)

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :fee
   belongs_to_active_hash :shipping_day
   
-  validates :name, :description, :price, :category, :condition, :fee, :region, :shipping_day, presence: true
+  validates :name, length: { maximum: 40 }, presence: true
+  validates :description, length: { maximum: 1000 }, presence: true
+  validates :price, numericality: {only_integer:true, greater_than: 299,less_than: 10000000} #numericalityは数値に関するvalidates
+  validates :category, :condition, :fee, :region, :shipping_day, presence: true
   validates :category_id, :condition_id, :fee_id, :region_id, :shipping_day_id, numericality: { other_than: 1 }
-  
 end
