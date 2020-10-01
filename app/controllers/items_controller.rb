d900820747942e7ca4c469515c8116108a1da1d0
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-10-01 17:19:36 +0900

    削除機能の実装
    #What
    削除機能を実装し、削除後はトップページに戻るように設定しました。
    #Why
    出品商品を出品者が削除できるようにする為

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
-  
-  before_action :set_item, only:[:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC") #includesメソッドはN+1問題を解消する
+    @items = Item.includes(:user).order('created_at DESC') # includesメソッドはN+1問題を解消する
   end
 
   def new
@@ -18,7 +17,7 @@ class ItemsController < ApplicationController
       render :new # newアクションを実行
     end
   end
-  
+
   def show
   end
 
@@ -33,7 +32,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :price, :category_id, :condition_id, :fee_id, :region_id, :shipping_day_id).merge(user_id: current_user.id)
   end
