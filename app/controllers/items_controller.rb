168f5dbdcf503d4b44a91c0f57d4a85073875f26
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-10-01 16:40:47 +0900

    updateアクションのメソッド修正
    before_actionに記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   
-  before_action :set_item, only:[:show, :edit]
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC") #includesメソッドはN+1問題を解消する
@@ -26,7 +26,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
