7e3b81ec52830284e0cd9ed5b20caa50f7a612e9
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-10-01 12:18:52 +0900

    item_controllerの修正
    -商品一覧を情報更新順に並び替え
    -includeメソッド(N+1問題を解消させるため)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.includes(:user).order("created_at DESC") #includesメソッドはN+1問題を解消する
   end
 
   def new
